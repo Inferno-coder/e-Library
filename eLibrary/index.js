150630e0c55f452c1fd545a6a58377d3719afd0a
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2022-05-29 00:19:14 +0530

    Add files via upload

---
+++ eLibrary/index.js
@@ -0,0 +1,50 @@
+//? navbar scroll
+/* let bar=document.querySelector(".navbar-toggler");
+bar.addEventListener("click",()=>{
+   let navList=document.querySelector(".navbar-collapse");
+   navList.classList.toggle("example");
+});
+  
+ */
+
+//? animating the text color 
+setInterval(()=>{
+   let para=document.querySelector(".spanText");
+   para.classList.toggle("aniText");
+   
+},3000)
+
+setInterval(()=>{
+    let brand=document.querySelector(".brand1");
+    
+    brand.classList.toggle("brand2");
+    let navAni=document.querySelector(".navbar-brand");
+    navAni.classList.toggle("navbar-brandAni")
+  
+    
+   },2000)
+
+//? scroll up the arrow mark to show the collection of books
+
+setInterval(()=>{
+   let scrollUp=document.querySelector(".scroll-up");
+   scrollUp.classList.toggle("scroll-ups");
+
+},300)
+
+//? making the password visible to the user
+
+let eye=document.getElementById("showPassword");
+eye.addEventListener("click",()=>{
+  let password=document.getElementById("password-in");
+  if(password.type==="password"){
+     password.type="text";
+  }
+  else{
+     password.type="password";
+  }
+}
+)
+
+
+
